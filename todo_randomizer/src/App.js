ce0762bfcbd73b434aa238118a12398e17303f15
Sora Harada <92529068+Saku052@users.noreply.github.com>
2024-04-07 17:04:12 +1000

    next commit

--- todo_randomizer/src/App.js
+++ todo_randomizer/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {// おしゃれなデザインのTodoアプリを作りたい
+    }
+      <h1>Todo Randomizer</h1>
+      <ul>
+        <li>Todo 1</li>
+        <li>Todo 2</li>
+        <li>Todo 3</li>
+      </ul>
     </div>
   );
 }
